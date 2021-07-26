68466857fd90fe3b60487a188eea2680922d6c69
vinayak098 <84779053+vinayak098@users.noreply.github.com>
2021-07-26 17:26:34 +0530

    Add files via upload

---
+++ js/Block.js
@@ -0,0 +1,36 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+            restitution :0.4,
+            friction :0.0,
+            isStatic : false
+        }
+        
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+         this.image= loadImage("block.png")
+        World.add(world, this.body);
+      }
+
+      display(){
+        console.log(this.body.speed);
+        var pos= this.body.position;
+        imageMode(CENTER);
+                  
+        if(this.body.speed <3){
+          image(this.image,pos.x,pos.y,this.width, this.height);
+        }
+        else{
+
+          World.remove(world, this.body)
+          push();
+          this.visiblity = this.visiblity - 1;
+          tint(255,this.visiblity);
+          image(this.image, this.body.position.x, this.body.position.y,this.width, this.height);
+          pop();
+        }
+
+
+        }
+}
